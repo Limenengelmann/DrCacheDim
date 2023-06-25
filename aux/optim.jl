575bc6be83bdd8b1847e2f0342f755ab837e8b06
elimtob <tobias.lima-engelmann@ericsson.com>
2023-06-25 20:49:31 +0200

    EOD

--- aux/optim.jl
+++ aux/optim.jl
@@ -2,22 +2,11 @@
 using YAML 
 using Printf
 
-#using LinearAlgebra
-#using JuMP
-#using Alpine
-#using Ipopt
-#
-## NLP optimizer
-#ipopt = optimizer_with_attributes(Ipopt.Optimizer,
-#                                        MOI.Silent() => true,
-#                                        "sb" => "yes",
-#                                        "max_iter"   => 9999)
-#
-## Global optimizer
-#alpine = optimizer_with_attributes(Alpine.Optimizer,
-#                                         "nlp_solver" => ipopt)
-#
-#m = Model(alpine)
+using LinearAlgebra
+using JuMP
+using Alpine
+using Ipopt
+using HiGHS
 
 @printf("[julia] Got %d args\n", length(ARGS))
 for a in ARGS
@@ -27,7 +16,6 @@ end
 
 const pSIM = ARGS[1]
 const pRES  = ARGS[2]
-
 
 const L1I = "L1I"
 const L1D = "L1D"
@@ -44,11 +32,11 @@ const SETS3 = 7
 const WAYS3 = 8
 
 function get_sets(H, lvl)
-    return H[lvl]["cfg"]["size"] / H[lvl]["cfg"]["assoc"]
+    return Int(log2(H[lvl]["cfg"]["size"] / H[lvl]["cfg"]["assoc"]))
 end
 
 function set_sets(H, lvl, s)
-    H[lvl]["cfg"]["size"] = s * H[lvl]["cfg"]["assoc"]
+    H[lvl]["cfg"]["size"] = 2^s * H[lvl]["cfg"]["assoc"]
 end
 
 function get_ways(H, lvl)
@@ -121,28 +109,81 @@ function run_cachesim(S)
     return R
 end
 
+function new_model(H0, Hmin, Hmax)
+    h0 = get_vec(H0)
+    hmin = get_vec(Hmin)
+    hmax = get_vec(Hmax)
+    # NLP optimizer
+    #ipopt = optimizer_with_attributes(Ipopt.Optimizer,
+    #                                    MOI.Silent() => true,
+    #                                    "sb" => "yes",
+    #                                    "max_iter"   => 9999)
+
+    ## Global optimizer
+    #alpine = optimizer_with_attributes(Alpine.Optimizer,
+    #                                     "nlp_solver" => ipopt)
+
+    M = Model(HiGHS.Optimizer)
+    #set_silent(M)
+    @variable(M, h[i=1:length(h0)], Int, start=h0[i])
+    # base constraints
+    @constraint(M, [i=1:length(h0)], hmin[i] <= h[i] <= hmax[i])
+    @constraint(M, h[SETS0] <= h[SETS1])
+    @constraint(M, h[SETS1] <= h[SETS2])
+    @constraint(M, h[SETS2] <= h[SETS3])
+    return M, h
+end
+
+function split(H, H_fa, H_dm)
+    #TODO simulate fully associative, direct mapped pendant to H
+    h = get_vec(H)
+    A = zeros(Int, 1, length(h))
+    # constraint H_i[SETS1] .>= H[SETS1]
+    A[SETS1] = 1
+    b = h[SETS1]    # TODO
+    return A, b
+end
+
 function solve()
     println("[julia] Reading H protype.")
     r = read(pRES, String)
-    println("[julia] Parsing..")
-    H = YAML.load(r)
-
-    H0 = H
+    H0 = YAML.load(r)
 
     Hmin = deepcopy(H0)
-    set_sets(Hmin, L1D, get_sets(H0, L1D)/2)
+    #FIXME realistic bounds
+    set_vec(Hmin, [4,1,4,1,4,1,4,1])
     Hmax = deepcopy(H0)
-    set_sets(Hmax, L1D, get_sets(H0, L1D)*2)
-    P0 = [Hmin, Hmax]   # TODO how to add constraints
-    #println(Hmin, Hmax)
+    #FIXME realistic bounds
+    set_vec(Hmax, [2^29,16,2^29,16,2^29,16,2^29,16])
+    # Define constraints as (A, b) s.t. A*h <= b
+    P0 = [Hmin, Hmax, [], []]   # TODO how to add constraints
 
     Problems = [P0]
     Simulated = []
 
     while length(Problems) > 0
         println("[julia] Checking new problem")
-        P = pop!(Problems)
+        h_min, h_max, A, b = popfirst!(Problems) #TODO FIFO
         #TODO pick direction (simulate fully associative/direct mapped pendant)
+
+        M, h = new_model(H0, Hmin, Hmax)
+        @objective(M, Max, sum(h))
+        optimize!(M)
+        @assert(termination_status(M) == OPTIMAL, "Could not find H_max")
+        solution_summary(M)
+        Hmax_new = value.(h)
+        #println(Hmax_new)
+
+        M, h = new_model(H0, Hmin, Hmax)
+        @objective(M, Min, sum(h))
+        optimize!(M)
+        @assert(termination_status(M) == OPTIMAL, "Could not find H_min")
+        solution_summary(M)
+        Hmin_new = value.(h)
+        #println(Hmin_new)
+
+        A_new, b_new = split(H, H, H)
+
         #TODO split into new problems (add new constraints)
         #TODO find new Hmin, Hmax, H0
         batch = []
